f78c656006338d7cac3868faccc73cb5fbeb2476
shankha117 <sinha.shankha17@gmail.com>
2022-08-04 01:41:37 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'data': {'name': 'shankha'}}
+
+
+@app.get('/about')
+def index():
+    return {'data': {'about': 'shankha_shankha'}}
